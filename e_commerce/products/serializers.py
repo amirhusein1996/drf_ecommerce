62cb5edb2b0df19e23bbe6637903a078df88db49
Amirhusein <amirhusein1996@gmail.com>
2023-07-30 22:23:35 +0330

    serializers created

---
+++ e_commerce/products/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+from .models import (
+    Product,
+    ProductImage,
+    Brand,
+    Category,
+)
+
+
+class BrandModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ('id', 'name', 'description', 'slug')
+        read_only_fields = ('id', 'slug')
+
+
+class CategoryModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'description', 'slug', 'parent')
+        read_only_fields = ('id', 'slug')
+        write_only_fields = ('parent',)
+
+
+class ProductImageModelSerializer(serializers.ModelSerializer):
+    pass
+
+
+class ProductDetailModelSerializer(serializers.ModelSerializer):
+    brand = BrandModelSerializer()
+    category = CategoryModelSerializer()
+    categories = serializers.SerializerMethodField()
+    images = ProductImageModelSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def get_categories(self, obj: Product):
+        serializer = CategoryModelSerializer(
+            obj.category.get_ancestors(include_self=True),
+            many=True
+        )
+        return serializer.data
+
+
+class ProductListModelSerializer(serializers.ModelSerializer):
+    brand = BrandModelSerializer()
+    category = CategoryModelSerializer()
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'short_description', 'price', 'main_image', 'brand', 'category', 'slug')
