bfb3cf7e4e4306d360e297080ce0ce6e0a1cabbc
Amirhusein <amirhusein1996@gmail.com>
2023-07-30 20:51:44 +0330

    jwt entry points is sets

---
+++ e_commerce/users/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from . import views
+
+
+app_name = 'users'
+
+jwt_urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
+
+app_urlpatterns = [
+
+]
+
+urlpatterns = jwt_urlpatterns + app_urlpatterns
