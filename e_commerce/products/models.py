1d600f1ecb7bff6d36fcd620d0eb19e7538577b4
Amirhusein <amirhusein1996@gmail.com>
2023-07-30 20:26:53 +0330

    slug field added to all models, also short_description to Product

--- e_commerce/products/models.py
+++ e_commerce/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 from mptt.models import MPTTModel, TreeForeignKey, TreeManyToManyField
 from e_commerce.core.models import SoftDeletation
@@ -7,10 +8,15 @@ from e_commerce.core.models import SoftDeletation
 class Brand(SoftDeletation):
     name = models.CharField(verbose_name=_('name'), max_length=255)
     description = models.TextField()
+    slug = models.SlugField(verbose_name=_('slug'), max_length=255, null=True, blank=True, editable=False)
 
     class Meta:
         verbose_name = _('Brand')
         verbose_name_plural = _('Brands')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -21,6 +27,7 @@ class Category(SoftDeletation, MPTTModel):
     description = models.TextField(verbose_name=_('description'))
     parent = TreeForeignKey(verbose_name=_('parent'), to='self', on_delete=models.PROTECT,
                             related_name=_('children'), blank=True, null=True)
+    slug = models.SlugField(verbose_name=_('slug'), max_length=255, null=True, blank=True, editable=False)
 
     class Meta:
         verbose_name = _('Category')
@@ -29,20 +36,30 @@ class Category(SoftDeletation, MPTTModel):
     class MPTTMeta:
         order_insertion_by = ['name']
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.name
 
 
 class Product(SoftDeletation):
     name = models.CharField(verbose_name=_('name'), max_length=255)
+    short_description = models.CharField(verbose_name=_('short description'), max_length=500)
     description = models.TextField(verbose_name=_('description'), blank=True)
     price = models.DecimalField(verbose_name=_('price'), max_digits=10, decimal_places=2)
     brand = models.ForeignKey(verbose_name=_('brand'), to=Brand, on_delete=models.PROTECT)
     category = TreeForeignKey(verbose_name=_('categories'), to=Category, related_name='products')
+    slug = models.SlugField(verbose_name=_('slug'), max_length=255, null=True, blank=True, editable=False)
 
     class Meta:
         verbose_name = _('Product')
         verbose_name_plural = _('Products')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
