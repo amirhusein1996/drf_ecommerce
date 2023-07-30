b1a47a25cac07e04eca86f29765273557c3099e1
Amirhusein <amirhusein1996@gmail.com>
2023-07-30 09:07:17 +0330

    user model defined

--- e_commerce/users/models.py
+++ e_commerce/users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), blank=True, unique=True)
+    age = models.PositiveSmallIntegerField(verbose_name=_('age'), blank=True)
+    avatar = models.ImageField(verbose_name=_('avatar'),upload_to='images/profiles', blank=True, null=True)
