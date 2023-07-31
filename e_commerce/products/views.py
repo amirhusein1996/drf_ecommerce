969d78df3f8ca4195c38d93b109df38207e7151a
Amirhusein <amirhusein1996@gmail.com>
2023-07-31 08:36:14 +0330

    ModelViewSets added

--- e_commerce/products/views.py
+++ e_commerce/products/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import (
+    CategoryModelSerializer,
+    BrandModelSerializer,
+    ProductDetailModelSerializer,
+    ProductListModelSerializer,
+    ProductImageModelSerializer,
+)
+from .models import (
+    Category,
+    Brand,
+    Product,
+    ProductImage,
+)
+from ..core.permissions import IsAdminOrReadOnly
 
-# Create your views here.
+
+class CategoryModelViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategoryModelSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class BrandModelViewSet(ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandModelSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class ProductModelViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ProductListModelSerializer
+        return ProductDetailModelSerializer
+
+
+class ProductImageModelViewSet(ModelViewSet):
+    queryset = ProductImage.objects.all()
+    serializer_class = ProductImageModelSerializer
+    permission_classes = [IsAdminOrReadOnly]
