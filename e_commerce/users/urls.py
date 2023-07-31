b69c622520fb635767aac4ddf34e4e5ea4754cd4
Amirhusein <amirhusein1996@gmail.com>
2023-07-31 15:37:32 +0330

    jwt tag added to their entry points

--- e_commerce/users/urls.py
+++ e_commerce/users/urls.py
@@ -1,22 +1,37 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
+from drf_spectacular.utils import extend_schema
 from . import views
-
 
 app_name = 'users'
 
 jwt_urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    # To organize these views by tags in Spectacular, we used the extend_schema decorator.
+
+    path(
+        'token/',
+        extend_schema(tags=['Simple JWT Authentication'])(TokenObtainPairView).as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'refresh/',
+        extend_schema(tags=['Simple JWT Authentication'])(TokenRefreshView).as_view(),
+        name='token_refresh'
+    ),
+    path(
+        'verify/',
+        extend_schema(tags=['Simple JWT Authentication'])(TokenVerifyView).as_view(),
+        name='token_verify'
+    ),
 ]
 
 app_urlpatterns = [
 
 ]
 
-urlpatterns = jwt_urlpatterns + app_urlpatterns
+urlpatterns: list = jwt_urlpatterns + app_urlpatterns
