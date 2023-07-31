93abae648635a6863ba2dce3ac25fbdf7c6adf70
Amirhusein <amirhusein1996@gmail.com>
2023-07-31 15:23:39 +0330

    name attr fixed

--- e_commerce/orders/models.py
+++ e_commerce/orders/models.py
@@ -64,7 +64,7 @@ class OrderItem(models.Model):
 
     class Meta:
         verbose_name = _('user'),
-        verbose_name_plural = _()
+        verbose_name_plural = _('users')
 
     def __str__(self):
         return '{}'.format(self.id)
