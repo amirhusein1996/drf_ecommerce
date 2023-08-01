8b11bff5f6ae3a46b7f82edcb5251613adb68612
Amirhusein <amirhusein1996@gmail.com>
2023-08-01 07:22:23 +0330

    views created

--- e_commerce/addresses/views.py
+++ e_commerce/addresses/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from drf_spectacular.views import extend_schema
+from .serializers import (
+    BillingAddressModelSerializer,
+    ShippingAddressModelSerializer
+    )
+from .models import (
+    BillingAddress,
+    ShippingAddress
+    )
 
-# Create your views here.
+
+@extend_schema(tags=['Billing Address'])
+class BillingAddressModelViewSet(ModelViewSet):
+    """
+    ## Viewset for managing billing addresses.
+
+    - Provides CRUD operations for the BillingAddress model, restricted to the
+        current authenticated user. Only authenticated users can access these views.
+    """
+
+    serializer_class = BillingAddressModelSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Return the queryset of ShippingAddress objects belonging to the current user
+        """
+        return BillingAddress.objects.filter(
+            user=self.request.user
+        )
+
+
+@extend_schema(tags=['Shipping Address'])
+class ShippingAddressModelViewSet(ModelViewSet):
+
+    """
+    ## Viewset for managing shipping addresses.
+
+    - Provides CRUD operations for the ShippingAddress model, restricted to the
+        current authenticated user. Only authenticated users can access these views.
+    """
+
+    serializer_class = ShippingAddressModelSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Return the queryset of ShippingAddress objects belonging to the current user
+        """
+        return ShippingAddress.objects.filter(
+            user=self.request.user
+        )
