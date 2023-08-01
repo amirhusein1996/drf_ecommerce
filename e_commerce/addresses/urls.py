c515a1dcf49f5594d7d400338595eb698129156a
Amirhusein <amirhusein1996@gmail.com>
2023-08-01 07:47:14 +0330

    router set

--- e_commerce/addresses/urls.py
+++ e_commerce/addresses/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import BillingAddressModelViewSet, ShippingAddressModelViewSet
 
 
-app_name= 'addresses'
+app_name = 'addresses'
 
-urlpatterns = [
+router = DefaultRouter()
+router.register('billing-addresses', BillingAddressModelViewSet, )
+router.register('shipping-addresses', ShippingAddressModelViewSet)
 
-]
+urlpatterns = router.urls
