e52854d25c3959981ef5f9e739de34d6a001a6c6
Amirhusein <amirhusein1996@gmail.com>
2023-08-01 09:00:24 +0330

    UserSerializer created

---
+++ e_commerce/users/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = [
+            '_old_email',
+            'is_staff',
+            'is_active',
+            'is_superuser',
+            'user_permissions',
+            'groups',
+            'date_joined',
+            'has_verified_email'
+        ]
+        read_only_fields = ['id']
+        write_only_fields = ['password']
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already exists")
+        return value
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            if key == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, key, value)
+        instance.save()
+        return instance
